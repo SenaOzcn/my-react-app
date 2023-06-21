66c3fcd2b25ad87df15bcbcd74bf3f2ba708dae0
Sena Nur Özcan <1.senaozcan@gmail.com>
2023-06-21 15:10:32 +0300

    jsx_expressions

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const myElement = React.createElement('h1', {}, 'I do not use JSX!');
+const myElement = <h1>React is {5 + 5} times better with JSX</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(myElement);
