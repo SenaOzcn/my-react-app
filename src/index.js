7c69d3be752d29d35731afa8295c7b5b62f8fc21
Sena Nur Özcan <1.senaozcan@gmail.com>
2023-06-22 01:40:29 +0300

    react_component_class

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Car from './Car.js';
+// import Car from './Car.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+class Car extends React.Component {
+    render() {
+        return <h2>Hi, I am a Car!</h2>
+    }
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
 root.render(<Car />);
 
 reportWebVitals();
