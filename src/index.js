c7ef28502a3eceef6044e879141a4c5a736756fe
Sena Nur Özcan <1.senaozcan@gmail.com>
2023-06-22 01:51:15 +0300

    react_class_components

--- src/index.js
+++ src/index.js
@@ -6,15 +6,23 @@ import reportWebVitals from './reportWebVitals';
 // import Car from './Car.js';
 
 class Car extends React.Component {
-    constructor(props) {
-        super(props);
-    }
     render() {
-        return <h2>I am a {this.props.model}!</h2>;
+        return <h2>I am a Car!</h2>;
+    }
+}
+
+class Garage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Who live in my Garage?</h1>
+                <Car />
+            </div>
+        );
     }
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Car model="Mustang" />);
+root.render(<Garage />);
 
 reportWebVitals();
