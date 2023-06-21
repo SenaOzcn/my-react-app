44c7ba162b437ece6dc661559c7bedc87ba42e6e
Sena Nur Özcan <1.senaozcan@gmail.com>
2023-06-21 15:07:59 +0300

    nojsx

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const myElement = <h1>I love JSX!</h1>;
+const myElement = React.createElement('h1', {}, 'I do not use JSX!');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(myElement);
