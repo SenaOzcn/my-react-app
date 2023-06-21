f604021e811c921e4340753acef1f24b6274d9cf
Sena Nur Özcan <1.senaozcan@gmail.com>
2023-06-21 15:05:02 +0300

    jsx1

--- src/index.js
+++ src/index.js
@@ -4,38 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// const myArray = ['apple', 'banana', 'orange'];
+const myElement = <h1>I love JSX!</h1>;
 
-// const myList = myArray.map((item) => <p>{item}</p>)
-
-// const container = document.getElementById('root');
-// const root = ReactDOM.createRoot(container);
-// root.render(myList);
-
-// const container = document.getElementById('root');
-// const root = ReactDOM.createRoot(container);
-// root.render(<p>Hello</p>)
-
-// const myElement = (
-//     <table>
-//         <tr>
-//             <th>Name</th>
-//         </tr>
-//         <tr>
-//             <td>John</td>
-//         </tr>
-//         <tr>
-//             <td>Elsa</td>
-//         </tr>
-//     </table>
-// );
-
-// const container = document.getElementById('root');
-// const root = ReactDOM.createRoot(container);
-// root.render(myElement);
-
-const container = document.getElementById('sandy');
-const root = ReactDOM.createRoot(container);
-root.render(<p>Hello</p>)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myElement);
 
 reportWebVitals();
