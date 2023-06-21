198396ada1d8aab3580b4e0dd194b0d63bdb0758
Sena Nur Özcan <1.senaozcan@gmail.com>
2023-06-21 15:12:52 +0300

    jsx_long

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const myElement = <h1>React is {5 + 5} times better with JSX</h1>;
+const myElement = (
+    <ul>
+        <li>Apples</li>
+        <li>Bananas</li>
+        <li>Cherries</li>
+    </ul>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(myElement);
