0781721f5b1d7e2bf358d1101cad47ad8e5bb591
Sena Nur Özcan <1.senaozcan@gmail.com>
2023-06-21 15:22:18 +0300

    jsx_classname

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const myElement = <input type="text" />;
+const myElement = <h1 className="myclass">Hello World!</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(myElement);
