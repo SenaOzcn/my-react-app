2c48149c3ba55bcf36b210a2ff31e4c9eb38d57b
Sena Nur Özcan <1.senaozcan@gmail.com>
2023-06-22 15:01:23 +0300

    react_lifecycle_render

--- src/index.js
+++ src/index.js
@@ -6,21 +6,14 @@ import reportWebVitals from './reportWebVitals';
 // import Car from './Car.js';
 
 class Header extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {favoritecolor: "red"};
-    }
-    static getDerivedStateFromProps(props, state) {
-        return {favoritecolor: props.favcol};
-    }
     render() {
         return (
-            <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+            <h1>This is the content of the Header component</h1>
         );
     }
 }
 
-const root = ReactDOM. createRoot(document.getElementById('root'));
-root.render(<Header favcol="yellow"/>);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Header />);
 
 reportWebVitals();
