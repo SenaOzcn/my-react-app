dc88542dc445cb68f31443b7d4eceb1228317105
Sena Nur Özcan <1.senaozcan@gmail.com>
2023-06-22 14:33:00 +0300

    react_state_setstate

--- src/Car.js
+++ src/Car.js
@@ -10,11 +10,15 @@ class Car extends React.Component {
             year: 1964
         };
     }
+    changeColor = () => {
+        this.setState({color: "blue"});
+    }
     render() {
         return (
             <div>
                 <h1>My Car is {this.state.brand}</h1>
                 <p>It is a {this.state.color} {this.state.model} from {this.state.year}.</p>
+                <button type="button" onClick={this.changeColor}>Change color</button>
             </div>
         );
     }
