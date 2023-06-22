c8aa430e5a601d33ec4bfe394c98c92b53882ac2
Sena Nur Özcan <1.senaozcan@gmail.com>
2023-06-22 14:54:54 +0300

    react_lifecycle_constructor

--- src/index.js
+++ src/index.js
@@ -3,9 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Car from './Car.js';
+// import Car from './Car.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Car />);
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {favoritecolor: "red"};
+    }
+    render() {
+        return (
+            <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+        );
+    }
+}
+
+const root = ReactDOM. createRoot(document.getElementById('root'));
+root.render(<Header />);
 
 reportWebVitals();
