889fa1b87a48f778511a8770552fbb2696dc142d
Sena Nur Özcan <1.senaozcan@gmail.com>
2023-06-22 15:06:19 +0300

    react_lifecycle_componentdidmount

--- src/index.js
+++ src/index.js
@@ -6,9 +6,18 @@ import reportWebVitals from './reportWebVitals';
 // import Car from './Car.js';
 
 class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {favoritecolor: "red"};
+    }
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({favoritecolor: "yellow"});
+        }, 1000)
+    }
     render() {
         return (
-            <h1>This is the content of the Header component</h1>
+            <h1>My Favorite Color is {this.state.favoritecolor}</h1>
         );
     }
 }
