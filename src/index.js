7e1d432f5617a23ed52a280953cc9ccdab44efff
Sena Nur Özcan <1.senaozcan@gmail.com>
2023-06-23 16:29:44 +0300

    react_props_pass3

--- src/index.js
+++ src/index.js
@@ -5,20 +5,23 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 function Car(props) {
-  return <h2>I am a { props.brand }!</h2>;
+  return <h2>I am a { props.brand.model }!</h2>
 }
 
 function Garage() {
-  const carName="Ford";
+  const carInfo= {
+    name: "Ford",
+    model: "Mustang"
+  };
   return(
     <>
-      <h1>Who lives in my Garage?</h1>
-      <Car brand={ carName} />
+      <h1>Who lives is my Garage?</h1>
+      <Car brand={ carInfo } />
     </>
   );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Garage />);
-  
+
 reportWebVitals();
