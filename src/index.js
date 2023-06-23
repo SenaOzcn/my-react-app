83799cc6b81c072c4b46af6735e74c0387acd0f4
Sena Nur Özcan <1.senaozcan@gmail.com>
2023-06-23 15:16:44 +0300

    react_lifecycle_getsnapshotbeforeupdate

--- src/index.js
+++ src/index.js
@@ -8,21 +8,27 @@ import reportWebVitals from './reportWebVitals';
 class Header extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {favoritecolor: "red"};
+        this.state = {favoriteColor: 'red'};
     }
-    shouldComponentUpdate() {
-        return true;
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({favoriteColor: 'green'})
+        }, 1000)
     }
-    changeColor = () => {
-        this.setState({favoritecolor: "blue"});
+    getSnapshotBeforeUpdate(prevProps,prevState) {
+        document.getElementById('div1').innerHTML = 'Before the update, the favorite was ' + prevState.favoriteColor;
+    }
+    componentDidUpdate() {
+        document.getElementById('div2').innerHTML = 'The updated favorite is ' + this.state.favoriteColor;
     }
     render() {
         return (
             <div>
-                <h1>My Favorite Color is {this.state.favoritecolor}</h1>
-                <button type="button" onClick={this.changeColor}>Change color</button>
+                <h1>My Favorite Color is {this.state.favoriteColor}</h1>
+                <div id='div1'></div>
+                <div id='div2'></div>
             </div>
-        );
+        )
     }
 }
 
